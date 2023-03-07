6ae066e22debc966bc5c798f2fb76ad256063556
Naoki-png <ct.anao38@outlook.jp>
2023-03-08 08:51:50 +0900

    hilt導入

--- app/src/main/java/com/example/speechapp/LoginFragment.kt
+++ app/src/main/java/com/example/speechapp/LoginFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.speechapp.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
